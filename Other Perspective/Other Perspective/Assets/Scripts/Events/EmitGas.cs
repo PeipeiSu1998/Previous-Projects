5ef2fa7eba641c09a8034eddda23649a616bb35a
Peipei Su <266128@via.dk>
2020-05-23 22:51:02 +0200

    project added

---
+++ Other Perspective/Other Perspective/Assets/Scripts/Events/EmitGas.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EmitGas : MonoBehaviour {
+
+    public ParticleSystem gas;
+    public static UnityEvent emitGas;
+    public AudioSource audioSrc;
+    public AudioClip gasSound;
+
+	// Use this for initialization
+	void Start () {
+		if(emitGas == null)
+        {
+            emitGas = new UnityEvent();
+        }
+        emitGas.AddListener(GasEmission);
+        emitGas.AddListener(GasEmissionSound);
+        
+	}
+	
+	void GasEmission()
+    {
+        print("gas emitted");
+        gas.Play();
+    }
+
+    void GasEmissionSound()
+    {
+        audioSrc.PlayOneShot(gasSound);
+    }
+}
---
+++ Other Perspective/Other Perspective/Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class GameStateManager : MonoBehaviour {
+
+    public GameObject glasses, bow;
+    public Animator fade;
+    private bool colorButtonsDone;
+	// Use this for initialization
+	void Start () {
+        colorButtonsDone = false;
+
+        if (ButtonAction.colorButtonEvent == null) ButtonAction.colorButtonEvent = new UnityEvent();
+		if(Chackpoint.OnKeyPuzzleDone == null)
+        {
+            Chackpoint.OnKeyPuzzleDone = new UnityEvent();
+        }
+        if (DoneTest.onMixingDone == null) DoneTest.onMixingDone = new UnityEvent();
+        DoneTest.onMixingDone.AddListener(FluidPuzzleDone);
+        Chackpoint.OnKeyPuzzleDone.AddListener(KeyPuzzleDone);
+        ButtonAction.colorButtonEvent.AddListener(OnGameDone);
+	}
+
+    private void KeyPuzzleDone()
+    {
+        Debug.Log("keyPuzzle Done!!");
+    }
+
+    private void FluidPuzzleDone()
+    {
+        Debug.Log("fluidPuzzle Done");
+        glasses.SetActive(true);
+        bow.SetActive(true);
+    }
+
+    private void OnDestroy()
+    {
+        Chackpoint.OnKeyPuzzleDone.RemoveListener(KeyPuzzleDone);
+        DoneTest.onMixingDone.RemoveListener(FluidPuzzleDone);
+    }
+
+    private void OnGameDone()
+    {
+        if (!colorButtonsDone)
+        {
+            Debug.Log("colorDone");
+            colorButtonsDone = true;
+            return;
+        }
+        Debug.Log("gamedone");
+        StartCoroutine(Restart());
+    }
+
+    IEnumerator Restart()
+    {
+        fade.SetBool("StartFadeToBlack", false);
+        fade.SetBool("StartFadeToBlack", true);
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(0);
+    }
+
+}
